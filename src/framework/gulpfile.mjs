b7fc506d237c61bf77afc86f7a8a6c472b34a70b
h2roverpete <pete@h2rover.com>
2026-09-22 19:54:55 -0700

    Add CSS injection scripts. Update styles.

--- src/framework/gulpfile.mjs
+++ src/framework/gulpfile.mjs
@@ -5,8 +5,9 @@ import RestAPI from './api/api.mjs'
 import * as fs from "node:fs";
 import 'dotenv/config';
 import mustache from "mustache";
-import {CloudFrontClient, CreateInvalidationCommand} from "@aws-sdk/client-cloudfront"; // ES Modules import
-
+import {CloudFrontClient, CreateInvalidationCommand} from "@aws-sdk/client-cloudfront";
+import https from "https"; // ES Modules import
+import path from "path";
 
 /**
  * Gulpfile for building sitemap and index.html in React public folder.
@@ -75,4 +76,28 @@ gulp.task("createInvalidation", async function () {
   const command = new CreateInvalidationCommand(input);
   const response = await client.send(command);
   console.log(`Response: ${JSON.stringify(response)}`.green);
+});
+
+
+gulp.task("injectCss", async function () {
+  const restApi = new RestAPI(
+    parseInt(process.env.REACT_APP_SITE_ID),
+    process.env.REACT_APP_BACKEND_HOST,
+    process.env.REACT_APP_API_KEY
+  );
+  const cssUrl = 'https://resources.h2rover.net/css/';
+  const htmlPath = './build/index.html';
+  const siteConfig = await restApi.getSite();
+  https.get(`${cssUrl}${siteConfig.SiteStyle}`, (res) => {
+    let css = '';
+    res.on('data', chunk => css += chunk);
+    res.on('end', () => {
+      if (fs.existsSync(htmlPath)) {
+        let html = fs.readFileSync(htmlPath, 'utf8');
+        html = html.replace('</head>', `<style>${css}</style></head>`);
+        fs.writeFileSync(htmlPath, html);
+        console.log('External CSS inlined into build/index.html');
+      }
+    });
+  });
 });
